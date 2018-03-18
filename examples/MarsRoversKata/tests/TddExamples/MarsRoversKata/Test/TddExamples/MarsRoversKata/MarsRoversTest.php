104624feda6e666bf95221490736b4f455700dc7
Miquel Fontana <miquelangel.fontana@enalquiler.com>
2018-03-18 12:44:21 +0100

    add the case when we just turn right

--- examples/MarsRoversKata/tests/TddExamples/MarsRoversKata/Test/TddExamples/MarsRoversKata/MarsRoversTest.php
+++ examples/MarsRoversKata/tests/TddExamples/MarsRoversKata/Test/TddExamples/MarsRoversKata/MarsRoversTest.php
@@ -28,4 +28,32 @@ class MarsRoversTest extends TestCase
         $this->assertSame($expectedPosition, $marsRovers->getCurrentPosition());
         $this->assertTrue($status, 'If the mars rovers doesn\'t find any obstacle, should return true');
     }
+
+    /**
+     * @test
+     */
+    public function whenWeJustTurnRightShouldHasEastDirection()
+    {
+        // Arrange
+        $originalPosition = [
+            'x' => 0,
+            'y' => 0,
+            'direction' => 'N'
+        ];
+        $expectedPosition = [
+            'x' => 0,
+            'y' => 0,
+            'direction' => 'E'
+        ];
+        $map = [];
+        $marsRovers = new MarsRovers($originalPosition, $map);
+        $commands = 'R';
+
+        // Act
+        $status = $marsRovers->move($commands);
+
+        // Assert
+        $this->assertSame($expectedPosition, $marsRovers->getCurrentPosition());
+        $this->assertTrue($status, 'If the mars rovers doesn\'t find any obstacle, should return true');
+    }
 }
