c32ee8e7149e234ff278c35c64aeb2c2167d1b04
Miquel Fontana <miquelangel.fontana@enalquiler.com>
2018-03-19 22:42:58 +0100

    finish test mars rovers

--- examples/MarsRoversKata/src/TddExamples/MarsRoversKata/MarsRovers.php
+++ examples/MarsRoversKata/src/TddExamples/MarsRoversKata/MarsRovers.php
@@ -5,19 +5,81 @@ namespace TddExamples\MarsRoversKata;
 
 class MarsRovers
 {
-    private $originalPosition;
+    private $position;
+
+    /**
+     * Map to pass from direction numeric to string.
+     *
+     * @var array
+     */
+    private $directionNumericToString = [
+        'N',
+        'E',
+        'S',
+        'W'
+    ];
+
+    /**
+     * For each direction the operation that we should do to update the position.
+     *
+     * @var array
+     */
+    private $directionToOperation = [
+        [ // N --> decrease by 1 the "Y"
+            'key'       => 'x',
+            'update'    => -1
+        ],
+        [ // E --> increment by 1 the "X"
+            'key'       => 'y',
+            'update'    => 1
+        ],
+        [ // S --> increment by 1 the "Y"
+            'key'       => 'x',
+            'update'    => 1
+        ],
+        [ // W --> decrease by 1 the "X"
+            'key'       => 'y',
+            'update'    => -1
+        ]
+    ];
+
+    private $limitsWorld = array(
+        'x' => 0,
+        'y' => 0
+    );
+    private $map;
 
     public function __construct($originalPosition, $map)
     {
-        $this->originalPosition = $originalPosition;
+        $this->map = $map;
+        $this->position = $originalPosition;
+        $this->position['direction'] = array_search($originalPosition['direction'], $this->directionNumericToString);
+        $this->limitsWorld['x'] = count( $map );
+        $this->limitsWorld['y'] = count( $map[0] );
     }
 
     public function move(string $commands): bool
     {
-        if ($commands === 'R') {
-            $this->originalPosition['direction'] = 'E';
-        } elseif ($commands === 'L') {
-            $this->originalPosition['direction'] = 'W';
+        $commands = strtolower($commands);
+        $totalCommands = strlen($commands);
+        for($i = 0; $i < $totalCommands; ++$i)
+        {
+            $command = $commands[$i];
+            try {
+                if ($command === 'r') {
+                    $this->position['direction'] = $this->updateDirection($this->position['direction'], +1);
+                } elseif ($command === 'l') {
+                    $this->position['direction'] = $this->updateDirection($this->position['direction'], -1);
+                } elseif($command === 'b') {
+                    $newPosition = $this->updatePosition($this->position, -1);
+                    $this->position = $newPosition;
+                } elseif($command === 'f') {
+                    $newPosition = $this->updatePosition($this->position, +1);
+                    $this->position = $newPosition;
+                }
+            } catch (\Exception $e) {
+                return false;
+            }
         }
 
         return true;
@@ -25,6 +87,49 @@ class MarsRovers
 
     public function getCurrentPosition(): array
     {
-        return $this->originalPosition;
+        return [
+            'x' => $this->position['x'],
+            'y' => $this->position['y'],
+            'direction' => $this->directionNumericToString[$this->position['direction']]
+        ];
+    }
+
+    private function updateDirection($currentDirection, $steps): int
+    {
+        $currentDirection = ($currentDirection + $steps)%4;
+
+        if ($currentDirection < 0){
+            $currentDirection += 4;
+        }
+
+        return $currentDirection;
+    }
+
+    private function updatePosition($currentPosition, $singMove): array
+    {
+        $operationAndKey = $this->directionToOperation[$currentPosition['direction']];
+        $currentPosition[$operationAndKey['key']] += ( $operationAndKey['update'] * $singMove );
+
+        $currentPosition = $this->correctOverWorld($currentPosition, $operationAndKey['key']);
+
+        if ( $this->map[$currentPosition['x']][$currentPosition['y']] != '.' )
+        {
+            throw new \Exception();
+        }
+
+
+        return $currentPosition;
+
+    }
+
+    private function correctOverWorld($currentPosition, $keyUpdated): array
+    {
+        if ($currentPosition[$keyUpdated] < 0)
+        {
+            $currentPosition[$keyUpdated] = $this->limitsWorld[$keyUpdated] - 1;
+        }
+        $currentPosition[$keyUpdated] = $currentPosition[$keyUpdated] % $this->limitsWorld[$keyUpdated];
+
+        return $currentPosition;
     }
 }
--- examples/MarsRoversKata/tests/TddExamples/MarsRoversKata/Test/TddExamples/MarsRoversKata/MarsRoversTest.php
+++ examples/MarsRoversKata/tests/TddExamples/MarsRoversKata/Test/TddExamples/MarsRoversKata/MarsRoversTest.php
@@ -18,7 +18,11 @@ class MarsRoversTest extends TestCase
             'y' => 0,
             'direction' => 'N'
         ];
-        $map = [];
+        $map = [
+            ['.', '.', '.', '.'],
+            ['.', '.', '.', '*'],
+            ['.', '.', '.', '*'],
+        ];
 
         $this->marsRovers = new MarsRovers($originalPosition, $map);
     }
@@ -90,8 +94,155 @@ class MarsRoversTest extends TestCase
         $this->assertMarsRoversHasNotColisioned($status);
     }
 
+    /**
+     * @test
+     */
+    public function whenWeMoveOnePositionToSudShouldIncreasedByOneX()
+    {
+        // Arrange
+        $expectedPosition = [
+            'x' => 1,
+            'y' => 0,
+            'direction' => 'N'
+        ];
+        $commands = 'B';
+
+        // Act
+        $status = $this->marsRovers->move($commands);
+
+        // Assert
+        $this->assertSame($expectedPosition, $this->marsRovers->getCurrentPosition());
+        $this->assertMarsRoversHasNotColisioned($status);
+    }
+
+    public function whenWeMoveManyCommandsCorrectlyProvider()
+    {
+        return [
+            'When we move one backward and turn left' => [
+                'commands' => 'bl',
+                'expectedPosition' => [
+                    'x' => 1,
+                    'y' => 0,
+                    'direction' => 'W'
+                ]
+            ],
+            'When we move to right and left we still facing north' => [
+                'commands' => 'rl',
+                'expectedPosition' => [
+                    'x' => 0,
+                    'y' => 0,
+                    'direction' => 'N'
+                ]
+            ],
+            'When we move to 4 right we still facing north' => [
+                'commands' => 'rrrr',
+                'expectedPosition' => [
+                    'x' => 0,
+                    'y' => 0,
+                    'direction' => 'N'
+                ]
+            ],
+            'When we move to 4 left we still facing north' => [
+                'commands' => 'llll',
+                'expectedPosition' => [
+                    'x' => 0,
+                    'y' => 0,
+                    'direction' => 'N'
+                ]
+            ],
+            'When we turn right and move one we should update position correctly' => [
+                'commands' => 'RF',
+                'expectedPosition' => [
+                    'x' => 0,
+                    'y' => 1,
+                    'direction' => 'E'
+                ]
+            ],
+            'When we advance one back and one frond we should not move our position' => [
+                'commands' => 'bf',
+                'expectedPosition' => [
+                    'x' => 0,
+                    'y' => 0,
+                    'direction' => 'N'
+                ]
+            ],
+            'When we move one front, we loop the world' => [
+                'commands' => 'f',
+                'expectedPosition' => [
+                    'x' => 2,
+                    'y' => 0,
+                    'direction' => 'N'
+                ]
+            ],
+            'When we move three back, we loop the world' => [
+                'commands' => 'bbb',
+                'expectedPosition' => [
+                    'x' => 0,
+                    'y' => 0,
+                    'direction' => 'N'
+                ]
+            ],
+            'When we turn left and move one we loop the world' => [
+                'commands' => 'lf',
+                'expectedPosition' => [
+                    'x' => 0,
+                    'y' => 3,
+                    'direction' => 'W'
+                ]
+            ],
+            'When we turn right and move four we loop the world' => [
+                'commands' => 'rffff',
+                'expectedPosition' => [
+                    'x' => 0,
+                    'y' => 0,
+                    'direction' => 'E'
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider whenWeMoveManyCommandsCorrectlyProvider
+     */
+    public function whenWeMoveManyCommandsCorrectly($commands, $expectedPosition)
+    {
+        // Act
+        $status = $this->marsRovers->move($commands);
+
+        // Assert
+        $this->assertSame($expectedPosition, $this->marsRovers->getCurrentPosition());
+        $this->assertMarsRoversHasNotColisioned($status);
+    }
+
+    /**
+     * @test
+     */
+    public function whenWeFindAnObstacleInThePathShouldStopMovingAndReturnFalse()
+    {
+        // Arrange
+        $expectedPosition = [
+            'x' => 1,
+            'y' => 2,
+            'direction' => 'E'
+        ];
+        $commands = 'RRfLffffff';
+
+        // Act
+        $status = $this->marsRovers->move($commands);
+
+        // Assert
+        $this->assertSame($expectedPosition, $this->marsRovers->getCurrentPosition());
+        $this->assertMarsRoversHasColisioned($status);
+    }
+
     private function assertMarsRoversHasNotColisioned($status)
     {
         $this->assertTrue($status, 'If the mars rovers doesn\'t find any obstacle, should return true');
+    }
+
+    private function assertMarsRoversHasColisioned($status)
+    {
+        $this->assertFalse($status, 'If the mars rovers find any obstacle, should return false');
     }
 }
