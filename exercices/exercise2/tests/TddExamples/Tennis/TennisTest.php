ce5fd5d598a93454c52dc6a72655a0f4a40a7d06
miquel-angel <miquelangel.fontana@enalquiler.com>
2018-03-20 20:47:45 +0100

    add some example in exercise2

--- exercices/exercise2/tests/TddExamples/Tennis/TennisTest.php
+++ exercices/exercise2/tests/TddExamples/Tennis/TennisTest.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 namespace Test\TddExamples\Tennis;
 
 use PHPUnit\Framework\TestCase;
+use TddExamples\Tennis\Tennis;
 
 class TennisTest extends TestCase
 {
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
 }
